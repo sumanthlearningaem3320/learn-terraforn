112b8b7cee79ec50b3dae00708431396a37ef4e2
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-04-15 08:20:37 +0530

    creating three servers and adding dns records

---
+++ expense-v1/main.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a85ba565da04fafd"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0538547HINC84GGOI9H"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a85ba565da04fafd"
+
+  tags = {
+    Name = "backend"
+  }
+}
+resource "aws_route53_record" "backend" {
+  zone_id = "Z0538547HINC84GGOI9H"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a85ba565da04fafd"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0538547HINC84GGOI9H"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
