6a90f5945b641e6203a3c135126eed4a2c8129f1
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-04-18 08:40:29 +0530

    for_each

--- 03-loops/main.tf
+++ 03-loops/main.tf
@@ -5,3 +5,26 @@ resource "null_resource" "sample1" {
   count = length(var.instance)
   //value = var.instance[count.index]
 }
+
+variable "instance1" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    frontend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+    frontend = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+resource "null_resource" "instance1" {
+  for_each = var.instance1
+  //count = length(var.instance)
+  //value = var.instance[count.index]
+}
