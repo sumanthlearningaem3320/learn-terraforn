044126f40ebac00181060ae5c3d1cc81ac357659
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-04-22 22:16:41 +0530

    expense-v2

--- moduleexpense-v4/expense/main.tf
+++ moduleexpense-v4/expense/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "instance" {
 
-  ami           = var.ami
+  ami            = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg_id
   tags = {
